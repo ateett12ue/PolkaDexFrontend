c0177d5ef0eda93f93101d339ea79743d91f1be8
ateet <ateet@propelld.com>
2022-12-03 23:08:31 +0530

    particle js updated

--- src/Components/Particle.jsx
+++ src/Components/Particle.jsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react'
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-
+import ParticleJson from './ParticleOption.json';
 const Particle = () => {
   const particlesInit = useCallback(async engine => {
     await loadFull(engine);
@@ -14,78 +14,7 @@ const particlesLoaded = useCallback(async container => {
     <Particles id="tsparticles"
     init={particlesInit}
     loaded={particlesLoaded}
-    options={{
-        background: {
-            color: {
-                value: "#28282B",
-            },
-        },
-        fpsLimit: 100,
-        interactivity: {
-            events: {
-                onClick: {
-                    enable: true,
-                    mode: "push",
-                },
-                onHover: {
-                    enable: true,
-                    mode: "repulse",
-                },
-                resize: true,
-            },
-            modes: {
-                push: {
-                    quantity: 2,
-                },
-                repulse: {
-                    distance: 100,
-                    duration: 0.4,
-                },
-            },
-        },
-        particles: {
-            color: {
-                value: "#C0C0C0",
-            },
-            links: {
-                color: "#C82B73",
-                distance: 150,
-                enable: true,
-                opacity: 0.5,
-                width: 1,
-            },
-            collisions: {
-                enable: true,
-            },
-            move: {
-                directions: "none",
-                enable: true,
-                outModes: {
-                    default: "bounce",
-                },
-                random: false,
-                speed: 6,
-                straight: false,
-            },
-            number: {
-                density: {
-                    enable: true,
-                    area: 600,
-                },
-                value: 30,
-            },
-            opacity: {
-                value: 0.6,
-            },
-            shape: {
-                type: "circle",
-            },
-            size: {
-                value: { min: 1, max: 5 },
-            },
-        },
-        detectRetina: true,
-    }}/>
+    options={ParticleJson}/>
   )
 }
 
